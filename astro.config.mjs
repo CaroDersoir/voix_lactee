356f987d6fc72db025dacd3f0c972683dd7b2be8
CaroDersoir <145647462+CaroDersoir@users.noreply.github.com>
2026-05-21 16:51:48 +0200

    fix base URL for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     site: 'https://CaroDersoir.github.io',
-    base: process.env.NODE_ENV === 'production' ? '/voix_lactee/' : '/',
+    base: process.env.NETLIFY ? '/' : (process.env.NODE_ENV === 'production' ? '/voix_lactee/' : '/'),
     vite: {
         plugins: [tailwindcss()]
     }
